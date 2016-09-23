8f1a68ea2a7a016660b6ddb4337020b224d7d75a
nbelonogov <nbelonogov@griddynamics.com>
2016-09-23 16:44:21 +0300

    refactoring

--- EarthViewWithGooglePhotoParser.sh
+++ EarthViewWithGooglePhotoParser.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
-    from=1000
-else
-    from=$1
-fi
-
-if [ -z $2 ]; then
-    to=2000
-else
-    to=$2
-fi
-
-if [ -z $3 ]; then
-    dir=/
-else
-    dir=$3
-fi
+from=${1:-1000}
+to=${2:-2000}
+dir=${3:-/}
 
 for i in $(seq $from $to); do
     wget -nc -nv -P $dir https://earthview.withgoogle.com/download/$i.jpg
