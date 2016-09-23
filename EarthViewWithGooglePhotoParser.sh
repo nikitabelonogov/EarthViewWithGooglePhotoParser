252c41dfe44ebda94325bd601cd8c325b928e972
nbelonogov <nbelonogov@griddynamics.com>
2016-09-23 16:21:27 +0300

    ars fix

--- EarthViewWithGooglePhotoParser.sh
+++ EarthViewWithGooglePhotoParser.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-from=1000
-to=1500
-
 if [ -z $1 ]; then
+    from=1000
+else
     from=$1
 fi
 
 if [ -z $2 ]; then
+    to=2000
+else
     to=$2
 fi
 
 for i in $(seq $from $to); do
-    wget -nc https://earthview.withgoogle.com/download/$i.jpg
+    wget -nc -nv https://earthview.withgoogle.com/download/$i.jpg
 done
