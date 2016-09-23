9221af29d2c0e268ca68cf86824213bc885fb683
nbelonogov <nbelonogov@griddynamics.com>
2016-09-23 16:32:02 +0300

    add directory arg

--- EarthViewWithGooglePhotoParser.sh
+++ EarthViewWithGooglePhotoParser.sh
@@ -12,6 +12,12 @@ else
     to=$2
 fi
 
+if [ -z $3 ]; then
+    dir=/
+else
+    dir=$3
+fi
+
 for i in $(seq $from $to); do
-    wget -nc -nv https://earthview.withgoogle.com/download/$i.jpg
+    wget -nc -nv -P $dir https://earthview.withgoogle.com/download/$i.jpg
 done
